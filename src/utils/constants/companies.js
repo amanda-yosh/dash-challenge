6b7a056420a4aa557029cfc2764b27ff9e9df695
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-11-06 13:59:37 -0300

    feat: create Companies panel

---
+++ src/utils/constants/companies.js
@@ -0,0 +1,8 @@
+import { SiIntercom, SiAsana, SiHubspot, SiAdafruit } from 'react-icons/si';
+
+export const companiesData = [
+  { logo: SiIntercom, name: 'Intercom', totalUsers: 329, tested: 0.935 },
+  { logo: SiAsana, name: 'Asana', totalUsers: 3216, tested: 0.134 },
+  { logo: SiHubspot, name: 'Hubspot', totalUsers: 251, tested: 0.250 },
+  { logo: SiAdafruit, name: 'Adafruit', totalUsers: 200, tested: 1.000 },
+];
